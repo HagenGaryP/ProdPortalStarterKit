ee9a53225e250c9e3b1802b75b19f21c201b99b0
HagenGaryP <hagengaryp@gmail.com>
2023-09-07 18:01:18 -0400

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@
 import React from 'react';
 import NavbarComponent from './components/Navbar/Navbar';
 import Home from './views/Home/Home';
+import Footer from './containers/Footer/Footer';
 
 const App: React.FC = () => {
   return (
     <>
       <NavbarComponent />
       <Home />
+      <Footer />
     </>
   );
 }
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import Hero from '../../containers/Hero/Hero';
 import ContentSection from '../../containers/ContentSection/ContentSection';
 
+import './Home.scss'
+
 const Home: React.FC = () => {
 
   return (
     <>
-    {/* sections of the home page */}
-    <Hero />
-    <ContentSection />
+      {/* sections of the home page */}
+      <div className="home-hero-wrapper">
+        <Hero />
+      </div>
+      <div className="home-content-wrapper">
+        <ContentSection />
+      </div>
     </>
   );
 }
