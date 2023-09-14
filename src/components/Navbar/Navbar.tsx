fc9eb5eb8bafd41ebc922b4417f6a70408c15df4
HagenGaryP <hagengaryp@gmail.com>
2023-09-14 16:09:06 -0400

    added router

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -21,11 +21,11 @@ const NavbarComponent: React.FC = () => {
     <>
       <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">Home</Navbar.Brand>
+          <Navbar.Brand href="/">Home</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#products">Products</Nav.Link>
+              <Nav.Link href="/products">Products</Nav.Link>
               <Nav.Link href="#pricing">Pricing</Nav.Link>
               <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,19 @@
 
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const root = document.getElementById('root');
 const reactRoot = createRoot(root!);
 
 reactRoot.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
